8054309f2326a1938e609fd14144bfb70a4606be
SlowKing <slowkinggg@vk.com>
2025-12-25 19:44:13 +0700

    feat: add URL normalization and validation functions

---
+++ page_analyzer/validator.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlparse
+
+import validators
+from flask_babel import gettext
+
+
+def normalize_url(url):
+    parsed_url = urlparse(url)
+    return f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+
+def validate_url(url):
+    if len(url) > 255:
+        return gettext("URL is too long.")
+
+    if not validators.url(url):
+        return gettext("Invalid URL.")
