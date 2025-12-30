54544cd221aac0e1a7b0aca40b6de12cc87d4396
SlowKing <slowkinggg@vk.com>
2025-12-30 16:31:38 +0700

    refactor: replace get_connection method with context manager for improved database handling. Refs #6.

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -2,44 +2,52 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 
+class db_context_manager:
+    def __init__(self, db_url):
+        self.db_url = db_url
+
+    def __enter__(self):
+        self.conn = psycopg2.connect(self.db_url)
+        self.cursor = self.conn.cursor(cursor_factory=DictCursor)
+        return self.conn, self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:
+            self.conn.rollback()
+        else:
+            self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+
+
 class UrlRepository:
     def __init__(self, db_url):
         self.db_url = db_url
 
-    def get_connection(self):
-        return psycopg2.connect(self.db_url)
-
     def add_url(self, url):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=DictCursor) as cursor:
-                cursor.execute(
-                    "INSERT INTO urls (name) VALUES (%s) RETURNING id", (url,)
-                )
-                row = cursor.fetchone()
-                conn.commit()
-                return row["id"] if row else None
+        with db_context_manager(self.db_url) as (conn, cursor):
+            cursor.execute(
+                "INSERT INTO urls (name) VALUES (%s) RETURNING id", (url,)
+            )
+            row = cursor.fetchone()
+            return row["id"] if row else None
 
     def get_url_by_id(self, url_id):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=DictCursor) as cursor:
-                cursor.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
-                row = cursor.fetchone()
-                return dict(row) if row else None
+        with db_context_manager(self.db_url) as (conn, cursor):
+            cursor.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
+            row = cursor.fetchone()
+            return dict(row) if row else None
 
     def get_url_by_name(self, url_name):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=DictCursor) as cursor:
-                cursor.execute(
-                    "SELECT * FROM urls WHERE name = %s", (url_name,)
-                )
-                row = cursor.fetchone()
-                return dict(row) if row else None
+        with db_context_manager(self.db_url) as (conn, cursor):
+            cursor.execute("SELECT * FROM urls WHERE name = %s", (url_name,))
+            row = cursor.fetchone()
+            return dict(row) if row else None
 
     def get_all_urls(self):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=DictCursor) as cursor:
-                cursor.execute(
-                    """SELECT DISTINCT ON (urls.id)
+        with db_context_manager(self.db_url) as (conn, cursor):
+            cursor.execute(
+                """SELECT DISTINCT ON (urls.id)
                         urls.id,
                         urls.name,
                         url_checks.status_code,
@@ -49,58 +57,53 @@ class UrlRepository:
                         ON urls.id = url_checks.url_id
                         ORDER BY urls.id DESC
                     """
-                )
-                rows = cursor.fetchall()
-                return [dict(row) for row in rows]
+            )
+            rows = cursor.fetchall()
+            return [dict(row) for row in rows]
 
     def add_url_check(self, url_id, status_code, h1, title, description):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=DictCursor) as cursor:
-                cursor.execute(
-                    """
+        with db_context_manager(self.db_url) as (conn, cursor):
+            cursor.execute(
+                """
                 INSERT INTO url_checks
                 (url_id, status_code, h1, title, description)
                 VALUES (%s, %s, %s, %s, %s)
                 """,
-                    (url_id, status_code, h1, title, description),
-                )
-                conn.commit()
+                (url_id, status_code, h1, title, description),
+            )
 
     def get_url_checks(self, url_id):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=DictCursor) as cursor:
-                cursor.execute(
-                    """
+        with db_context_manager(self.db_url) as (conn, cursor):
+            cursor.execute(
+                """
                     SELECT * FROM url_checks
                     WHERE url_id = %s
                     ORDER BY id DESC
                     """,
-                    (url_id,),
-                )
-                rows = cursor.fetchall()
-                return [dict(row) for row in rows]
+                (url_id,),
+            )
+            rows = cursor.fetchall()
+            return [dict(row) for row in rows]
 
     def get_last_url_check(self, url_id):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=DictCursor) as cursor:
-                cursor.execute(
-                    """
+        with db_context_manager(self.db_url) as (conn, cursor):
+            cursor.execute(
+                """
                     SELECT *
                     FROM url_checks
                     WHERE url_id = %s
                     ORDER BY created_at DESC
                     LIMIT 1
                     """,
-                    (url_id,),
-                )
-                row = cursor.fetchone()
-                return dict(row) if row else None
+                (url_id,),
+            )
+            row = cursor.fetchone()
+            return dict(row) if row else None
 
     def get_url_check_by_id(self, check_id):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=DictCursor) as cursor:
-                cursor.execute(
-                    "SELECT * FROM url_checks WHERE id = %s", (check_id,)
-                )
-                row = cursor.fetchone()
-                return dict(row) if row else None
+        with db_context_manager(self.db_url) as (conn, cursor):
+            cursor.execute(
+                "SELECT * FROM url_checks WHERE id = %s", (check_id,)
+            )
+            row = cursor.fetchone()
+            return dict(row) if row else None
